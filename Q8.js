ffb28084e59dca7204e916c590da747a6c64134e
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-12-19 17:33:17 +0000

    question 8 alternative added

--- Q8.js
+++ Q8.js
@@ -1,5 +1,17 @@
+// function isThisAPalindrome(str) {
+//   const reversedStr = str.split("").reverse().join("");
+
+//   return str === reversedStr;
+// }
+
 function isThisAPalindrome(str) {
-  const reversedStr = str.split("").reverse().join("");
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
 
   return str === reversedStr;
 }
+
+console.log(isThisAPalindrome("nitrogen"));
